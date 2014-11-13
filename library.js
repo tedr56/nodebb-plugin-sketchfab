dcc8c1bf56b676576e1c082064026ef8f13d2f5b
TedVJ <tedr56@gmail.com>
2014-11-13 16:35:16 +0100

    Correct Regex

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var Sketchfab = {},
-		embed = '<iframe class="sketchfab-embed" frameborder="0" height="480" width="854" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" src="http://sketchfab.com/embed/$1?autostart=0&transparent=0&autospin=0&controls=1"></iframe>';
+		embed = '<iframe class="sketchfab-embed" frameborder="0" height="480" width="854" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" src="http://sketchfab.com/embed/$1\?autostart=0&transparent=0&autospin=0&controls=1"></iframe>';
 
 
 
@@ -10,7 +10,8 @@ http://sketchfab.com/embed/bd6ed505658e4601850bf36f0abb3bf8
 
 	Sketchfab.parse = function(postContent, callback) {
 		// modified from http://stackoverflow.com/questions/7168987/
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:sketchfab\.com)\/?(?:models)\/?(.+)<\/a>/g, embed);
+		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:sketchfab\.com)\/?(?:models)\/?(.+)">.+<\/a>/g, embed);
+//                                          /<a href="(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com)                 \/?(.+)">.+<\/a>/g
 		callback(null, postContent);
 	};
 
